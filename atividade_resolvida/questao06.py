f530eaaa3c4824d840469d3631d769986f034a23
Edilton Saraiva <ediltonsaraiva@gmail.com>
2026-04-03 15:04:41 -0300

    Correção questão 6

--- atividade_resolvida/questao06.py
+++ atividade_resolvida/questao06.py
@@ -1,28 +1,17 @@
 import random
 
 print()
-print("-" * 25)
-print("-----CARTELA DE BINGO----")
-print("-" * 25)
+print("-" * 26)
+print("-----CARTELA DE BINGO-----")
+print("-" * 26)
 
-
-linhas_bingo = 5
-colunas_bingo = 5
+linhas_bingo, colunas_bingo = 5, 5
 cartela_bingo = [int] * linhas_bingo
 
 for linha_indice in range(len(cartela_bingo)):
     cartela_bingo[linha_indice] = [int] * colunas_bingo
 
-numeros_bingo = [int] * 100
-
-for numero in range(len(numeros_bingo)):
-    numeros_bingo[numero] = random.randint(0, 99)
-
-numeros_sorteados = [int] * 25
-
-for sorte in range(len(numeros_bingo)):
-    numeros_sorteados = random.sample(numeros_bingo, 25) 
-
+numeros_sorteados = random.sample(range(100), 25)
 contador = 0
 
 for linhas_da_cartela in range(5):
@@ -31,8 +20,8 @@ for linhas_da_cartela in range(5):
         contador += 1
 
 for linha_de_impressao in cartela_bingo:
-    for num in linha_de_impressao:
-        print(f"{num:2d}", end = "   ")
+    for coluna_de_impressao in linha_de_impressao:
+        print(f"{coluna_de_impressao:2d}", end = "    ")
     print()
     print()
 print()
